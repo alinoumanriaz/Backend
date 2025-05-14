cd7f56f58c1003a2801052d9a1d49398f78608f2
alinouman <alinomanriazch@gmail.com>
2025-05-14 19:05:32 +0500

    updates

--- client.js
+++ client.js
@@ -8,16 +8,13 @@ const redis = createClient({
   url: process.env.REDIS_URL, // should be redis://default:pass@redis.railway.internal:6379
 });
 
-let isConnected = false;
-
 redis.on('error', (err) => {
   console.error('❌ Redis error:', err);
 });
 
 export const getRedisClient = async () => {
-  if (!isConnected) {
+  if (!redis.isOpen) {
     await redis.connect();
-    isConnected = true;
     console.log('✅ Redis connected');
   }
   return redis;
